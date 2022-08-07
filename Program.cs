cddef9008f4a6d7ae2671a8bb47c25e937e416f2
Linar <alr86@mail.ru>
2022-08-08 00:03:34 +0300

    added program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿string[] ShortWords(string[] text)
+{
+string[] textNew = new string[text.Length];
+int index = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    if(text[i].Length <= 3)
+    {
+        textNew[index] = text[i];
+        index++;
+    }
+}
+return textNew;
+}
+
+void ShowArray(string[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+        Console.Write(a[i] + " ");
+}
+
+string[] text = {"a", "ab", "abc", "abcd", "abcde"};
+ShowArray(ShortWords(text));
